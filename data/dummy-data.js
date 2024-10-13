726a33e466960d11eb820db9531ebda5c562a7e4
KHUSHI 444 <khankhushi4452@gmail.com>
2024-10-13 22:34:36 +0530

    changes

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -6,7 +6,10 @@ export const CATEGORIES = [
     new Category('C2', 'Italian', '#2596be'),
     new Category('C3', 'Italian', '#f5428d'), 
     new Category('C4', 'Italian', '#f5428d'),
-    new Category('C5', 'Italian', '#f5428d'),
-    new Category('C6', 'Italian', '#f5428d'),
+    new Category('C5', 'Italian', '#2596be'),
+    new Category('C6', 'Italian', '#f5428d'), 
+    
+    
+    
     
 ]
