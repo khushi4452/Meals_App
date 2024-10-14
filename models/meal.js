1d81e725439bf6ac836c47311af526f3fad78000
KHUSHI 444 <khankhushi4452@gmail.com>
2024-10-14 23:37:11 +0530

    meals page added

---
+++ models/meal.js
@@ -0,0 +1,18 @@
+class Meal {
+    constructor(
+        id,
+        title,
+        affordability,
+        complexity,
+        imageurl,
+        duration,
+        imgradients,
+        steps,
+        isVegan,
+        isvegeterian
+    ){
+
+     
+
+    }
+}
