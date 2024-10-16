9e7220d0a4e875758a46764ed54aa3cc42089706
KHUSHI 444 <khankhushi4452@gmail.com>
2024-10-16 23:09:23 +0530

    file added to the page

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,2 @@
+function CategoryGridTile {}
+export default CategoryGridTile;
--- screen/CategoriesScreen.js
+++ screen/CategoriesScreen.js
@@ -1,7 +1,12 @@
  import { FlatList } from 'react-native';
  import { CATEGORIES } from '../data/dummy-data.js';
+ function renderCategoryItem (item) {
+return ;
+ }
 function CategoriesScreen() {
- return 
+ return  <FlatList data={CATEGORIES}
+  keyExtractor={ (item)=> item.id}
+  renderItem= {renderCategoryItem}/>
 }
 
 export default CategoriesScreen;
