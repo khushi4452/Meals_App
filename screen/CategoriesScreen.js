9b354ead43b0bae2e5506a0ee9e54b6c1dec4bbd
KHUSHI 444 <khankhushi4452@gmail.com>
2024-10-18 23:21:53 +0530

    app.js feature changes

--- screen/CategoriesScreen.js
+++ screen/CategoriesScreen.js
@@ -2,7 +2,7 @@
  import { CATEGORIES } from '../data/dummy-data.js';
 import CategoryGridTile from '../components/CategoryGridTile.js';
  function renderCategoryItem (item) {
-return <CategoryGridTile  title={item.title}/>
+return <CategoryGridTile  title={itemData.item.title} color={itemData.item.color}/>
  }
 function CategoriesScreen() {
  return <FlatList data={CATEGORIES}
