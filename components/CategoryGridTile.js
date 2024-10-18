0e47c574cbc368ea029a0b8b8de1a3d4ecd893c8
KHUSHI 444 <khankhushi4452@gmail.com>
2024-10-18 23:31:55 +0530

    container css design

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,7 +1,7 @@
 import { Pressable,View,Text,StyleSheet } from "react-native";
 
 function CategoryGridTile({title , color}) {
-    return <View>
+    return <View style={styles.gridItem}>
         <Pressable>
         <View>
             <Text>{title}</Text>
@@ -13,6 +13,16 @@ export default CategoryGridTile;
 
 const styles = StyleSheet.create({
    gridItem: {
-    flex: 1
-   } 
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4
+   },
+   innerContainer : {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center'
+   }
 });
