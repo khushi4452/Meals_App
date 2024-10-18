0d80149fbef336e28b43d873bc8dbba2fd1a226c
KHUSHI 444 <khankhushi4452@gmail.com>
2024-10-18 23:14:00 +0530

    basic structure of Category

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,9 +1,11 @@
 import { Pressable } from "react-native";
 
-function CategoryGridTile {
+function CategoryGridTile({title , color}) {
     return <View>
         <Pressable>
-            
+        <View>
+            <Text>{title}</Text>
+        </View>
         </Pressable>
     </View>
 }
