0b7c1a307a97a46aee697cda8bda97ec69f2f18b
Musrat Khan <147485713+khushi4452@users.noreply.github.com>
2024-10-19 22:38:33 +0530

    Update dummy-data.js

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -3,8 +3,8 @@ import meal from '../models/meal';
 
 export const CATEGORIES = [
     new Category('C1', 'Italian', '#f5428d'),
-    new Category('C2', 'Italian', '#2596be'),
-    new Category('C3', 'Italian', '#f5428d'), 
+    new Category('C2', 'Indian', '#2596be'),
+    new Category('C3', 'vegitarian', '#f5428d'), 
     new Category('C4', 'Italian', '#f5428d'),
     new Category('C5', 'Italian', '#2596be'),
     new Category('C6', 'Italian', '#f5428d'), 
